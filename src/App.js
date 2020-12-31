f555ed2241d3fe48053e55757db703fdcd020abc
okutaku <pntgn.taku@gmail.com>
2020-12-31 15:11:10 +0900

    stateのオブジェクトの使い方

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-// import Counter from './components/Counter'
-import CounterHooks from './components/CounterHook'
+// import Counter from './components/Counter';
+// import CounterHooks from './components/CounterHook';
+import FormHook from './components/FormHook'
 
 function App() {
   return (
     <div className="App">
       {/* <Counter /> */}
-      <CounterHooks />
+      {/* <CounterHooks /> */}
+      <FormHook />
     </div>
   );
 }
---
+++ src/components/FormHook.js
@@ -0,0 +1,31 @@
+//オブジェクトの扱い方
+//オブジェクトの置換がされるため、スプレット構文を用いて、もとのオブジェクトを再度作成する
+
+import React, {useState} from 'react'
+
+function FormHook() {
+  const [name, setName] = useState({ firstName: '', lastName: '',})
+
+  return (
+    <div>
+      <div>
+        <p>First Name: - {name.firstName}</p>
+        <p>Last Name: - {name.lastName}</p>
+      </div>
+      <form>
+        <input 
+        type="text" 
+        value={name.firstName}
+        onChange={e => setName({...name, firstName: e.target.value})}
+        />
+        <input 
+        type="text" 
+        value={name.lastName}
+        onChange={e => setName({...name, lastName: e.target.value})}
+        />
+      </form>
+    </div>
+  )
+}
+
+export default FormHook

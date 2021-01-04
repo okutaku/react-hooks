498f2edcba04efd0346484b99dca2d07b3599550
okutaku <pntgn.taku@gmail.com>
2021-01-04 11:00:05 +0900

    useRefの使い方

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import FocusInput from './components/Foucusinout';
+import Count from './components/Count2';
 
 function App() {
   
   return (
     <div className="App">
       <h1>useRef</h1>
-      <FocusInput/>
+      <Count />
     </div>
   );
 }
--- src/components/Foucusinout.js
+++ src/components/Foucusinout.js
@@ -15,5 +15,7 @@ function FocusInput(){
 
 export default FocusInput
 
-//useRef
+//useRefの使い方
 //JSX内のノードに対してミュータブルな状態にし、アクションを起こせるようにする
+//domを操作したい場合
+//クロージャー内でアクセスしたい値を使いたいとき
